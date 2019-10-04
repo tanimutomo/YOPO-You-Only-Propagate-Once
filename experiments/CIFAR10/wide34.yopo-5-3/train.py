a62d8d21a95e91abcc150c10346b26f595d3a9ee
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-10-04 13:41:49 +0900

    Add comments

--- experiments/CIFAR10/wide34.yopo-5-3/train.py
+++ experiments/CIFAR10/wide34.yopo-5-3/train.py
@@ -24,57 +24,75 @@ torch.backends.cudnn.benchmark = True
 
 writer = SummaryWriter(log_dir=config.log_dir)
 
+# WideResNet34
 net = create_network()
 net.to(DEVICE)
+
+# CrossEntropyLoss
 criterion = config.create_loss_function().to(DEVICE)
 #criterion = CrossEntropyWithWeightPenlty(net.other_layers, DEVICE, config.weight_decay)#.to(DEVICE)
 #ce_criterion = nn.CrossEntropyLoss().to(DEVICE)
+
+# SGD Optimizer
 optimizer = config.create_optimizer(net.other_layers.parameters())
+
+# MultiStepLR Scheduler
 lr_scheduler = config.create_lr_scheduler(optimizer)
 
-
 ## Make Layer One trainner  This part of code should be writen in config.py
-
 Hamiltonian_func = Hamiltonian(net.layer_one, config.weight_decay)
 layer_one_optimizer = optim.SGD(net.layer_one.parameters(), lr = lr_scheduler.get_lr()[0], momentum=0.9, weight_decay=5e-4)
-lyaer_one_optimizer_lr_scheduler = optim.lr_scheduler.MultiStepLR(layer_one_optimizer,
+layer_one_optimizer_lr_scheduler = optim.lr_scheduler.MultiStepLR(layer_one_optimizer,
                                                                   milestones = [30, 34, 36], gamma = 0.1)
 LayerOneTrainer = FastGradientLayerOneTrainer(Hamiltonian_func, layer_one_optimizer,
                                               config.inner_iters, config.sigma, config.eps)
 
-
-
+# dataset
 ds_train = create_train_dataset(args.batch_size)
 ds_val = create_test_dataset(args.batch_size)
 
+# attacker for evaluation
 #TrainAttack = config.create_attack_method(DEVICE)
 EvalAttack = config.create_evaluation_attack_method(DEVICE)
 
 now_epoch = 0
 
+# resume training
 if args.auto_continue:
     args.resume = os.path.join(config.model_dir, 'last.checkpoint')
 if args.resume is not None and os.path.isfile(args.resume):
     now_epoch = load_checkpoint(args.resume, net, optimizer,lr_scheduler)
 
 while True:
+    # check and count epoch
     if now_epoch > config.num_epochs:
         break
     now_epoch = now_epoch + 1
 
+    # report
     descrip_str = 'Training epoch:{}/{} -- lr:{}'.format(now_epoch, config.num_epochs,
-                                                                       lr_scheduler.get_lr()[0])
+                                                         lr_scheduler.get_lr()[0])
+
+    # training
     acc, yofoacc = train_one_epoch(net, ds_train, optimizer, criterion, LayerOneTrainer, config.K,
                     DEVICE, descrip_str)
+
+    # report
     tb_train_dic = {'Acc':acc, 'YofoAcc':yofoacc}
     print(tb_train_dic)
     writer.add_scalars('Train', tb_train_dic, now_epoch)
+
+    # validation phase
     if config.val_interval > 0 and now_epoch % config.val_interval == 0:
+        # validate
         acc, advacc = eval_one_epoch(net, ds_val, DEVICE, EvalAttack)
+
+        # report 
         tb_val_dic = {'Acc': acc, 'AdvAcc': advacc}
         writer.add_scalars('Val', tb_val_dic, now_epoch)
 
+    # update model weights
     lr_scheduler.step()
-    lyaer_one_optimizer_lr_scheduler.step()
+    layer_one_optimizer_lr_scheduler.step()
     save_checkpoint(now_epoch, net, optimizer, lr_scheduler,
                     file_name = os.path.join(config.model_dir, 'epoch-{}.checkpoint'.format(now_epoch)))
